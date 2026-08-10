f614e74d45e1558361ca2147438bf1de5a96c2bb
pranavgugale561-prog <pranavgugale561@gmail.com>
2026-08-10 19:27:32 +0530

    chore: debug server load error

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-const app = require('../server/index.js'); module.exports = app;
+try {
+  const app = require('../server/index.js');
+  module.exports = app;
+} catch (error) {
+  module.exports = (req, res) => {
+    res.status(500).json({ error: "Failed to load server/index.js", message: error.message, stack: error.stack });
+  };
+}
