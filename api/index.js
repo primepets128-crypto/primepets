8dc7797228f10174b542ace286f20350d06f91db
pranavgugale561-prog <pranavgugale561@gmail.com>
2026-08-10 19:29:22 +0530

    fix: downgrade firebase-admin to v11 to fix Vercel ESM require error

--- api/index.js
+++ api/index.js
@@ -1,8 +1,2 @@
-try {
-  const app = require('../server/index.js');
-  module.exports = app;
-} catch (error) {
-  module.exports = (req, res) => {
-    res.status(500).json({ error: "Failed to load server/index.js", message: error.message, stack: error.stack });
-  };
-}
+const app = require('../server/index.js');
+module.exports = app;
